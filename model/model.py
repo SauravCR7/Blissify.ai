689c5cdc97561d244a43e205cd43356e5f86b0dc
Abhilash Pal <abhilashpal8@gmail.com>
2018-03-23 19:28:34 +0530

    basic model

---
+++ model/model.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
